0f4d2a6137208ce3fe45652b0eeb0dc3bd3f6e66
bugman <onlyforwin@gmail.com>
2019-07-05 17:35:58 +0200

    FIX: update runs

--- truffle-config.js
+++ truffle-config.js
@@ -14,14 +14,19 @@ module.exports = {
       version: "0.5.2",
       optimizer: {
         enabled: true,
-        runs: 200
+        runs: 1
       }
     }
   },
   networks: {
     local: {
       host: '127.0.0.1',
-      port: 9545,
+      port: 8545,
+      network_id: '*'
+    },
+    test: {
+      host: '127.0.0.1',
+      port: 8545,
       network_id: '*'
     },
     ropsten: {
