7fd57212e38b641a780641217fd641c9fa548804
bugman <onlyforwin@gmail.com>
2019-07-10 12:10:05 +0200

    FIX: TransactionsCard copy and structure

--- client/src/utilities/components/TransactionsCard.js
+++ client/src/utilities/components/TransactionsCard.js
@@ -24,7 +24,12 @@ class TransactionsCard extends React.Component {
     return (
       <Flex px={4} mx={'auto'} flexDirection={'column'}>
         <Text fontWeight={3} mb={3}>
-          Activity:
+          <Text textAlign={"center"} p={3}>
+            {Object.keys(this.props.transactions).length < 1 ?
+              "No activity yet. Lend some assets to start a transaction. " :
+              "Activity:"
+            }
+          </Text>
         </Text>
 
         <TransactionTable>
@@ -39,86 +44,78 @@ class TransactionsCard extends React.Component {
                 <th>txHash</th>
               </tr>
             ) : null}
-        </thead>
+          </thead>
 
-            {Object.keys(this.props.transactions).length < 1 ? (
-              <Text textAlign={"center"} p={3}>
-                No transactions yet. Lend some assets to start a transaction.
-              </Text>
-            ) : (
-              <tbody>
-                {Object.keys(this.props.transactions).map((keyName, keyIndex) => {
-                  let txHash = "";
-                  if (this.props.transactions[keyName].transactionHash) {
-                    txHash = this.props.transactions[
-                      keyName
-                    ].transactionHash.toString();
-                    // const txStart = txHash.substr(0, 7);
-                    // const txEnd = txHash.substr(txHash.length - 4);
-                    // txHash = txStart + "..." + txEnd;
-                  }
+          <tbody>
+            {Object.keys(this.props.transactions).map((keyName, keyIndex) => {
+              let txHash = "";
+              if (this.props.transactions[keyName].transactionHash) {
+                txHash = this.props.transactions[
+                  keyName
+                ].transactionHash.toString();
+                // const txStart = txHash.substr(0, 7);
+                // const txEnd = txHash.substr(txHash.length - 4);
+                // txHash = txStart + "..." + txEnd;
+              }
 
-                  let eventCreated = new Date(this.props.transactions[keyName].created);
-                  let eventUpdated = new Date(this.props.transactions[keyName].lastUpdated);
-                  const method = this.props.transactions[keyName].method;
-                  let humanMethod;
+              let eventCreated = new Date(this.props.transactions[keyName].created);
+              let eventUpdated = new Date(this.props.transactions[keyName].lastUpdated);
+              const method = this.props.transactions[keyName].method;
+              let humanMethod;
 
-                  switch (method) {
-                    case 'mintIdleToken':
-                      humanMethod = 'Lend'
-                      break;
-                    case 'redeemIdleToken':
-                      humanMethod = 'Redeem'
-                      break;
-                    default:
-                      humanMethod = method;
-                  }
-                  const status = this.props.transactions[keyName].status;
-                  let color;
-                  switch (status) {
-                    case 'success':
-                      color = 'green';
-                      break;
-                    case 'error':
-                      color = 'red';
-                      break;
-                    default:
-                      color = 'dark-gray';
-                  }
-                  return (
-                    <tr key={keyIndex}>
-                      <td>
-                        <code>
-                          {humanMethod}
-                        </code>
-                      </td>
-                      <td>
-                        <Pill color={color}>
-                          {status}
-                        </Pill>
-                      </td>
-                      <td>
-                        {eventCreated.toDateString()}
-                      </td>
-                      <td>
-                        {eventUpdated.toTimeString()}
-                      </td>
-                      <td>
-                        {this.props.transactions[keyName].confirmationCount}
-                      </td>
-                      <td>
-                        <Link href={'https://mainnet.etherscan.io/tx/'+txHash} target='_blank'>
-                          {txHash}
-                        </Link>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            )}
-
+              switch (method) {
+                case 'mintIdleToken':
+                  humanMethod = 'Lend'
+                  break;
+                case 'redeemIdleToken':
+                  humanMethod = 'Redeem'
+                  break;
+                default:
+                  humanMethod = method;
+              }
+              const status = this.props.transactions[keyName].status;
+              let color;
+              switch (status) {
+                case 'success':
+                  color = 'green';
+                  break;
+                case 'error':
+                  color = 'red';
+                  break;
+                default:
+                  color = 'dark-gray';
+              }
+              return (
+                <tr key={keyIndex}>
+                  <td>
+                    <code>
+                      {humanMethod}
+                    </code>
+                  </td>
+                  <td>
+                    <Pill color={color}>
+                      {status}
+                    </Pill>
+                  </td>
+                  <td>
+                    {eventCreated.toDateString()}
+                  </td>
+                  <td>
+                    {eventUpdated.toTimeString()}
+                  </td>
+                  <td>
+                    {this.props.transactions[keyName].confirmationCount}
+                  </td>
+                  <td>
+                    <Link href={'https://mainnet.etherscan.io/tx/'+txHash} target='_blank'>
+                      {txHash}
+                    </Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
         </TransactionTable>
-
       </Flex>
     );
   }
