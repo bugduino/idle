f4ad51870617fdcdbf70bf818b03c51e47f11bbc
bugman <onlyforwin@gmail.com>
2019-07-31 22:03:06 +0200

    WIP: try to calculate the new interest rate on compound after supplying assets

--- buidler.config.js
+++ buidler.config.js
@@ -42,6 +42,186 @@ task("cDAI:rate", "cDAI to DAI rate")
     console.log(`RES: ${res.toString()}`)
   });
 
+task("cDAI:nextRateData", "cDAI to DAI next rate data")
+  .setAction(async taskArgs => {
+    const cERC20 = artifacts.require('CERC20');
+    const ERC20 = artifacts.require('ERC20');
+    const WhitePaperInterestModel = artifacts.require('WhitePaperInterestModel');
+
+    const cDAI = await cERC20.at('0xf5dce57282a584d2746faf1593d3121fcac444dc'); // mainnet
+    const cDAIWithSupply = await ERC20.at('0xf5dce57282a584d2746faf1593d3121fcac444dc'); // mainnet
+    const whitePaperInterestModel = await WhitePaperInterestModel.at('0xd928c8ead620bb316d2cefe3caf81dc2dec6ff63'); // mainnet
+
+
+    let promises = [
+      whitePaperInterestModel.getBorrowRate.call(
+        await cDAI.getCash.call(),
+        await cDAI.totalBorrows.call(),
+        BNify(0)
+        // await cDAI.totalReserves.call()
+      ),
+      cDAI.supplyRatePerBlock.call(),
+      cDAI.borrowRatePerBlock.call(),
+
+      cDAI.totalBorrows.call(),
+      cDAI.getCash.call(),
+      cDAI.totalReserves.call(),
+      cDAIWithSupply.totalSupply.call(),
+      cDAI.reserveFactorMantissa.call(),
+    ];
+
+    const res = await Promise.all(promises);
+    const [whiteBorrow, contractSupply, contractBorrow, totalBorrows, getCash, totalReserves, totalSupply, reserveFactorMantissa] = res;
+
+    borrowRatePerYear = BNify(whiteBorrow[1]).times('2102400').times('100').integerValue(BigNumber.ROUND_FLOOR)
+    supplyRatePerYear = BNify(contractSupply).times('2102400').times('100').integerValue(BigNumber.ROUND_FLOOR)
+    borrowRatePerYearContract = BNify(contractBorrow).times('2102400').times('100').integerValue(BigNumber.ROUND_FLOOR)
+
+    console.log(`${whiteBorrow[1].toString()} borrow whitepaper`);
+    console.log(`${contractSupply.toString()} contract supply`);
+    console.log(`${contractBorrow.toString()} contract borrow`);
+    console.log(`################`);
+    console.log(`${BNify(borrowRatePerYear).div(1e18).toString()}% borrowRatePerYear white`);
+    console.log(`${BNify(borrowRatePerYearContract).div(1e18).toString()}% borrowRatePerYear contract`);
+    console.log(`${BNify(supplyRatePerYear).div(1e18).toString()}% supplyRatePerYear`);
+    console.log(`################`);
+    console.log(`${BNify(totalBorrows).div(1e18).toString()} totalBorrows`)
+    console.log(`${BNify(getCash).div(1e18).toString()} getCash`)
+    console.log(`${BNify(totalReserves).div(1e18).toString()} totalReserves`)
+    console.log(`${BNify(totalSupply).div(1e8).toString()} totalSupply`)
+    console.log(`${BNify(reserveFactorMantissa).toString()} reserveFactorMantissa`)
+    console.log(`################`);
+
+
+    // const newDAIAmount = BNify(1000).times(1e18);
+    // TODO here we should add the new DAI amount we are adding;
+    // const newCash = BNify(getCash).plus(newDAIAmount);
+    // const newSupply =
+
+    const rate = BNify(getCash).plus(BNify(totalBorrows)).minus(BNify(totalReserves)).div(BNify(totalSupply)).times(1e18);
+    // const rate = BNify(newCash).plus(BNify(totalBorrows)).minus(BNify(totalReserves)).div(BNify(newSupply)).times(1e18);
+    console.log(`${BNify(rate).toString()} rate`);
+
+
+    const underlying = BNify(totalSupply).times(rate.div(1e18));
+    console.log(`${BNify(underlying).toString()} underlying`);
+    const borrowsPer = BNify(totalBorrows).div(underlying);
+    console.log(`${BNify(borrowsPer).toString()} borrowsPer`);
+    const borrowRate = whiteBorrow[1];
+    console.log(`${BNify(borrowRate).toString()} borrowRate`);
+    // const borrowRate = await whitePaperInterestModel.getBorrowRate.call(newCash, totalBorrows, BNify(0)),
+
+    // TODO 1-reserveFactorMantissa have different udm
+    const oneMinusReserveFactor = BNify(1e18).minus(reserveFactorMantissa);
+    console.log(`${BNify(oneMinusReserveFactor).toString()} oneMinusReserveFactor`);
+
+    const supplyRate = BNify(borrowRate).times(oneMinusReserveFactor).times(borrowsPer).div(1e18);
+    console.log(`${BNify(supplyRate).toString()} supplyRate`);
+
+    const newSupplyRatePerYear = BNify(supplyRate).times('2102400').times('100').integerValue(BigNumber.ROUND_FLOOR)
+    console.log(`${BNify(newSupplyRatePerYear).div(1e18).toString()}% newSupplyRatePerYear`);
+
+
+
+    // function supplyRatePerBlock() external view returns (uint) {
+    //     /* We calculate the supply rate:
+    //      *  underlying = totalSupply × exchangeRate
+    //      *  borrowsPer = totalBorrows ÷ underlying
+    //      *  supplyRate = borrowRate × (1-reserveFactor) × borrowsPer
+    //      */
+  });
+
+  task("cDAI:nextRateDataWithAmount", "cDAI to DAI next rate data")
+    .setAction(async taskArgs => {
+      const cERC20 = artifacts.require('CERC20');
+      const ERC20 = artifacts.require('ERC20');
+      const WhitePaperInterestModel = artifacts.require('WhitePaperInterestModel');
+
+      const cDAI = await cERC20.at('0xf5dce57282a584d2746faf1593d3121fcac444dc'); // mainnet
+      const cDAIWithSupply = await ERC20.at('0xf5dce57282a584d2746faf1593d3121fcac444dc'); // mainnet
+      const whitePaperInterestModel = await WhitePaperInterestModel.at('0xd928c8ead620bb316d2cefe3caf81dc2dec6ff63'); // mainnet
+
+      const newDAIAmount = BNify(100000000).times(1e18);
+      const getCashPre = await cDAI.getCash.call();
+      const amount = BNify(getCashPre.toString()).plus(newDAIAmount);
+      const totalBorrowsPre = await cDAI.totalBorrows.call();
+
+      const whiteBorrow = await whitePaperInterestModel.getBorrowRate.call(
+        web3.utils.toBN(amount),
+        totalBorrowsPre,
+        BNify(0)
+        // await cDAI.totalReserves.call()
+      );
+
+      let promises = [
+        Promise.resolve({ok: 'ok'}),
+        cDAI.supplyRatePerBlock.call(),
+        cDAI.borrowRatePerBlock.call(),
+
+        cDAI.totalBorrows.call(),
+        cDAI.getCash.call(),
+        cDAI.totalReserves.call(),
+        cDAIWithSupply.totalSupply.call(),
+        cDAI.reserveFactorMantissa.call(),
+        cDAI.exchangeRateStored.call(),
+      ];
+
+      const res = await Promise.all(promises);
+      const [foo, contractSupply, contractBorrow, totalBorrows, getCash, totalReserves, totalSupply, reserveFactorMantissa, exchangeRateStored] = res;
+
+      borrowRatePerYear = BNify(whiteBorrow[1]).times('2102400').times('100').integerValue(BigNumber.ROUND_FLOOR)
+      supplyRatePerYear = BNify(contractSupply).times('2102400').times('100').integerValue(BigNumber.ROUND_FLOOR)
+      borrowRatePerYearContract = BNify(contractBorrow).times('2102400').times('100').integerValue(BigNumber.ROUND_FLOOR)
+
+      console.log(`${whiteBorrow[1].toString()} borrow whitepaper`);
+      console.log(`${contractSupply.toString()} contract supply`);
+      console.log(`${contractBorrow.toString()} contract borrow`);
+      console.log(`################`);
+      console.log(`${BNify(borrowRatePerYear).div(1e18).toString()}% borrowRatePerYear white`);
+      console.log(`${BNify(borrowRatePerYearContract).div(1e18).toString()}% borrowRatePerYear contract`);
+      console.log(`${BNify(supplyRatePerYear).div(1e18).toString()}% supplyRatePerYear`);
+      console.log(`################`);
+      console.log(`${BNify(totalBorrows).div(1e18).toString()} totalBorrows`)
+      console.log(`${BNify(getCash).div(1e18).toString()} getCash`)
+      console.log(`${BNify(totalReserves).div(1e18).toString()} totalReserves`)
+      console.log(`${BNify(totalSupply).div(1e8).toString()} totalSupply`)
+      console.log(`${BNify(reserveFactorMantissa).toString()} reserveFactorMantissa`)
+      console.log(`${BNify(exchangeRateStored).toString()} exchangeRateStored`)
+      console.log(`################`);
+
+
+      // TODO here we should add the new DAI amount we are adding;
+      const newCash = BNify(getCash).plus(newDAIAmount);
+      const newCDAI = newDAIAmount.times(1e18).div(exchangeRateStored).div(1e8);
+      console.log(`${BNify(newCDAI).toString()} newCDAI`);
+      const newSupply = BNify(totalSupply).plus(newCDAI);
+
+      // const rate = BNify(getCash).plus(BNify(totalBorrows)).minus(BNify(totalReserves)).div(BNify(totalSupply)).times(1e18);
+      const rate = BNify(newCash).plus(BNify(totalBorrows)).minus(BNify(totalReserves)).div(BNify(newSupply)).times(1e18);
+      console.log(`${BNify(rate).toString()} rate`);
+
+      // ################
+
+      const underlying = BNify(newSupply).times(rate.div(1e18));
+      // const underlying = BNify(totalSupply).times(rate.div(1e18));
+      console.log(`${BNify(underlying).toString()} underlying`);
+      const borrowsPer = BNify(totalBorrows).div(underlying);
+      console.log(`${BNify(borrowsPer).toString()} borrowsPer`);
+      const borrowRate = whiteBorrow[1];
+      console.log(`${BNify(borrowRate).toString()} borrowRate`);
+      // const borrowRate = await whitePaperInterestModel.getBorrowRate.call(newCash, totalBorrows, BNify(0)),
+
+      // TODO 1-reserveFactorMantissa have different udm
+      const oneMinusReserveFactor = BNify(1e18).minus(reserveFactorMantissa);
+      console.log(`${BNify(oneMinusReserveFactor).toString()} oneMinusReserveFactor`);
+
+      const supplyRate = BNify(borrowRate).times(oneMinusReserveFactor).times(borrowsPer).div(1e18);
+      console.log(`${BNify(supplyRate).toString()} supplyRate`);
+
+      const newSupplyRatePerYear = BNify(supplyRate).times('2102400').times('100').integerValue(BigNumber.ROUND_FLOOR)
+      console.log(`${BNify(newSupplyRatePerYear).div(1e18).toString()}% newSupplyRatePerYear`);
+    });
+
 task("cDAI:apr", "Get apr")
   .setAction(async taskArgs => {
     const cERC20 = artifacts.require('CERC20');
@@ -69,7 +249,7 @@ module.exports = {
   },
   networks: {
     develop: {
-      url: `https://ropsten.infura.io/v3/${INFURA_KEY}`
+      url: `https://mainnet.infura.io/v3/${INFURA_KEY}`
     }
   }
 };
--- contracts/interfaces/CERC20.sol
+++ contracts/interfaces/CERC20.sol
@@ -5,4 +5,10 @@ interface CERC20 {
   function redeem(uint256 redeemTokens) external returns (uint256);
   function exchangeRateStored() external view returns (uint256);
   function supplyRatePerBlock() external view returns (uint256);
+  function borrowRatePerBlock() external view returns (uint256);
+
+  function totalReserves() external view returns (uint256);
+  function getCash() external view returns (uint256);
+  function totalBorrows() external view returns (uint256);
+  function reserveFactorMantissa() external view returns (uint256);
 }
