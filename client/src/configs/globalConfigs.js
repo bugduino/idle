529322939163441d186827002f094c25e4116769
Samster <cester.samuele@gmail.com>
2020-05-10 23:47:25 +0200

    FIX: Clear localStorage data except walletProvider and connectorName if version has changed

--- client/src/configs/globalConfigs.js
+++ client/src/configs/globalConfigs.js
@@ -6,7 +6,7 @@ const env = process.env;
 
 const globalConfigs = {
   appName: 'Idle',
-  version: 'v2.2',
+  version: 'v2.3',
   baseURL: 'https://idle.finance',
   baseToken: 'ETH',
   countries:{
