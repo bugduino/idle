93425c4a2fd8ad390d12df844a70801966adb049
Samster <cester.samuele@gmail.com>
2020-06-26 18:20:17 +0200

    FIX: updated client version

--- client/src/configs/globalConfigs.js
+++ client/src/configs/globalConfigs.js
@@ -16,7 +16,7 @@ const env = process.env;
 
 const globalConfigs = {
   appName: 'Idle',
-  version: 'v3.4',
+  version: 'v3.5',
   baseURL: 'https://idle.finance',
   baseToken: 'ETH',
   countries:{
