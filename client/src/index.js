5f0105b3414954fc490d6cfabbcb2127264172e6
Samster <cester.samuele@gmail.com>
2020-07-07 11:38:01 +0200

    FIX: filter errors before sending to Sentry

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,19 @@ import App from './App/App';
 import * as serviceWorker from './serviceWorker';
 
 if (window.location.hostname !== 'localhost') {
-  Sentry.init({dsn: "https://56f5c0bce273442390d7f25698fefe53@sentry.io/1547544"});
+  Sentry.init({
+    dsn: "https://56f5c0bce273442390d7f25698fefe53@sentry.io/1547544",
+    beforeSend(event, hint) {
+      let output = event;
+      const error = hint.originalException;
+      if (error && error.message){
+        if (error.message.match(/Failed to subscribe to new newBlockHeaders/i)){
+          output = false;
+        }
+      }
+      return output;
+    }
+  });
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
