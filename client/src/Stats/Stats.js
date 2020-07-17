2894bc2d68f262afbfaa4179eef308f8034f2b4d
Samster <cester.samuele@gmail.com>
2020-07-17 11:44:35 +0200

    FIX: added granularity in Stats

--- client/src/Stats/Stats.js
+++ client/src/Stats/Stats.js
@@ -277,7 +277,7 @@ class Stats extends Component {
 
 
     const isRisk = this.state.idleVersion === 'v3' && this.props.selectedStrategy === 'risk';
-    let apiResults_unfiltered = await this.functionsUtil.getTokenApiData(this.props.tokenConfig.address,isRisk,startTimestamp,endTimestamp,true);
+    let apiResults_unfiltered = await this.functionsUtil.getTokenApiData(this.props.tokenConfig.address,isRisk,startTimestamp,endTimestamp,true,7200);
 
     const apiResults = this.filterTokenData(apiResults_unfiltered);
 
