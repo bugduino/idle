0e9e6bf44ac632259abc7879c1bdf32b6e6f4088
Samster <cester.samuele@gmail.com>
2020-09-30 11:20:16 +0200

    FIX: cleaned curve files

--- client/src/Breadcrumb/Breadcrumb.js
+++ client/src/Breadcrumb/Breadcrumb.js
@@ -14,42 +14,48 @@ class Breadcrumb extends Component {
         id={'breadcrumb'}
         alignItems={'center'}
       >
-       <RoundIconButton
-         buttonProps={{
-           width:'35px',
-           height:'35px'
-         }}
-         iconSize={'1.2em'}
-         iconName={'ArrowBack'}
-         handleClick={this.props.handleClick}
-       />
-       <Link
-         ml={[2,3]}
-         fontSize={2}
-         fontWeight={3}
-         color={'cellText'}
-         hoverColor={'copyColor'}
-         onClick={this.props.handleClick}
-       >
-        {this.props.text}
-       </Link>
-       {
-        (!this.props.isMobile || showPathMobile) && this.props.path && this.props.path.length>0 &&
-          this.props.path.map((path,index) => (
-           <Text
-             pl={[1,3]}
-             ml={[1,3]}
+        {
+          this.props.handleClick && 
+           <RoundIconButton
+             buttonProps={{
+               width:'35px',
+               height:'35px'
+             }}
+             iconSize={'1.2em'}
+             iconName={'ArrowBack'}
+             handleClick={this.props.handleClick}
+           />
+        }
+        {
+          this.props.text && this.props.text.length>0 &&
+           <Link
+             ml={[2,3]}
+             fontSize={2}
              fontWeight={3}
-             fontSize={[1,2]}
-             color={'statValue'}
-             key={`breadcrumb_path_${index}`}
-             borderLeft={`1px solid ${theme.colors.divider}`}
+             color={'cellText'}
+             hoverColor={'copyColor'}
+             onClick={this.props.handleClick}
            >
-             {path}
-           </Text>
-          ))
-       }
-     </Flex>
+            {this.props.text}
+           </Link>
+        }
+        {
+          (!this.props.isMobile || showPathMobile) && this.props.path && this.props.path.length>0 &&
+            this.props.path.map((path,index) => (
+                <Text
+                pl={[1,3]}
+                ml={[1,3]}
+                fontWeight={3}
+                fontSize={[1,2]}
+                color={'statValue'}
+                key={`breadcrumb_path_${index}`}
+                borderLeft={`1px solid ${theme.colors.divider}`}
+              >
+                {path}
+              </Text>
+            ))
+        }
+      </Flex>
     );
   }
 }
