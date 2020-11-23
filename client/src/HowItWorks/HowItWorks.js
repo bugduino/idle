88c4a625db3a3a886fac4963960463cfa4f9fca1
Samster91 <cester.samuele@gmail.com>
2020-11-23 15:50:29 +0100

    FIX: font size in how it works cards

--- client/src/HowItWorks/HowItWorks.js
+++ client/src/HowItWorks/HowItWorks.js
@@ -53,10 +53,10 @@ class HowItWorks extends Component {
                 <Image width={['2.5em','3.3em']} src={'images/how-it-works/choose-strategy.svg'} />
               </Flex>
               <Box width={1}>
-                <Heading.h3 textAlign={'center'} fontFamily={'sansSerif'} fontSize={[3,3]} mb={1} color={'blue'}>
+                <Heading.h3 textAlign={'center'} fontFamily={'sansSerif'} fontSize={[3,3]} mb={[1,2]} color={'blue'}>
                   Choose your strategy
                 </Heading.h3>
-                <Heading.h4 fontSize={[2,2]} px={[1,0]} textAlign={'center'} fontWeight={2} lineHeight={1.5}>
+                <Heading.h4 fontSize={1} px={[1,0]} textAlign={'center'} fontWeight={2} lineHeight={1.5}>
                   Build your portfolio with different allocation strategies that aim to maximize your returns and keeping you in your risk comfort zone
                 </Heading.h4>
               </Box>
@@ -80,10 +80,10 @@ class HowItWorks extends Component {
                 <Image width={['2.5em','3.3em']} src={'images/how-it-works/deposit-stablecoins.svg'} />
               </Flex>
               <Box width={1}>
-                <Heading.h3 textAlign={'center'} fontFamily={'sansSerif'} fontSize={[3,3]} mb={1} color={'blue'}>
+                <Heading.h3 textAlign={'center'} fontFamily={'sansSerif'} fontSize={[3,3]} mb={[1,2]} color={'blue'}>
                   Deposit your crypto-assets
                 </Heading.h3>
-                <Heading.h4 fontSize={[2,2]} px={[1,0]} textAlign={'center'} fontWeight={2} lineHeight={1.5}>
+                <Heading.h4 fontSize={1} px={[1,0]} textAlign={'center'} fontWeight={2} lineHeight={1.5}>
                   Just deposit and relax. Your funds will be automatically allocated among DeFi protocols and you will immediately start earning interest.
                 </Heading.h4>
               </Box>
@@ -107,10 +107,10 @@ class HowItWorks extends Component {
                 <Image width={['2.5em','3.3em']} src={'images/how-it-works/rebalance.svg'} />
               </Flex>
               <Box width={1}>
-                <Heading.h3 textAlign={'center'} fontFamily={'sansSerif'} fontSize={[3,3]} mb={1} color={'blue'}>
+                <Heading.h3 textAlign={'center'} fontFamily={'sansSerif'} fontSize={[3,3]} mb={[1,2]} color={'blue'}>
                   Automated Rebalancing
                 </Heading.h3>
-                <Heading.h4 fontSize={[2,2]} px={[1,0]} textAlign={'center'} fontWeight={2} lineHeight={1.5}>
+                <Heading.h4 fontSize={1} px={[1,0]} textAlign={'center'} fontWeight={2} lineHeight={1.5}>
                   Idle automatically keeps the appropriate allocation mix, depending on the strategy. Idle consistently checks for better opportunities.
                 </Heading.h4>
               </Box>
@@ -133,10 +133,10 @@ class HowItWorks extends Component {
                 <Image width={['2.5em','3.3em']} src={'images/how-it-works/insights-redeem.svg'} />
               </Flex>
               <Box width={1}>
-                <Heading.h3 textAlign={'center'} fontFamily={'sansSerif'} fontSize={[3,3]} mb={1} color={'blue'}>
+                <Heading.h3 textAlign={'center'} fontFamily={'sansSerif'} fontSize={[3,3]} mb={[1,2]} color={'blue'}>
                   Easy insights and redeem
                 </Heading.h3>
-                <Heading.h4 fontSize={[2,2]} px={[1,0]} textAlign={'center'} fontWeight={2} lineHeight={1.5}>
+                <Heading.h4 fontSize={1} px={[1,0]} textAlign={'center'} fontWeight={2} lineHeight={1.5}>
                   Monitor your funds’ performance and rebalance events, see your estimated earnings and easlily redeem back your funds + interest.
                 </Heading.h4>
               </Box>
