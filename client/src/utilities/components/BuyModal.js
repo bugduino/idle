c6ff44b7785b793f8a7a3da8ee90973625bd5418
Samster91 <cester.samuele@gmail.com>
2020-11-27 01:07:07 +0100

    FIX: removed unnecessary escape char

--- client/src/utilities/components/BuyModal.js
+++ client/src/utilities/components/BuyModal.js
@@ -491,7 +491,7 @@ class BuyModal extends React.Component {
               }
               <Text textAlign={'center'} fontWeight={500} fontSize={[2, this.props.showInline ? 2 : 3]} my={0}>
                 {
-                  this.props.showInline && this.props.buyToken ? `Ops... you don\'t have any ${this.props.buyToken} in your wallet, how do you prefer do buy some?` : `How do you prefer to buy ${this.state.selectedToken}?`
+                  this.props.showInline && this.props.buyToken ? `Ops... you don't have any ${this.props.buyToken} in your wallet, how do you prefer do buy some?` : `How do you prefer to buy ${this.state.selectedToken}?`
                 }
               </Text>
             </Flex>
